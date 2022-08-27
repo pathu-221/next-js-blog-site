9832f4f4b6e6f9e090005c5f518bdcb79ef1a91f
pratham aggarwal <prathamaggarwal23@gmail.com>
2022-08-28 00:05:18 +0530

    added toaster for notifications

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,16 @@ import styles from '../styles/Home.module.css'
 
 import Link from 'next/link';
 
+import {Button} from '@mui/material';
+
+import toast from 'react-hot-toast';
+
 import Loader from '@components/Loader';
 
 export default function Home() {
   return (
-    <div> 
-      <Loader show/>
+    <div>
+        <Button onClick={() => toast.success('hello toast')}>Toast me</Button>
     </div>
   )
 }
