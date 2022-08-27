bcc480e30358d69f299dd5739084fa912ae0cd3a
pratham aggarwal <prathamaggarwal23@gmail.com>
2022-08-27 21:38:40 +0530

    added loader

---
+++ components/Loader.js
@@ -0,0 +1,5 @@
+function Loader() {
+    return show ? <div className='loader'></div> : null;
+}
+
+export default Loader;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div> 
-      
+      <Link prefetch={false} href={{
+        pathname: '/[username]',
+        query: {username: 'pratham22'},
+      }}>
+        <a>My profile</a>
+      </Link>
     </div>
   )
 }
